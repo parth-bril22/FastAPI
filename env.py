61a38497df5f9090071d8399c3459cad6daf588a
Parth Patel <100412963+parth-bril22@users.noreply.github.com>
2022-04-15 17:58:40 +0530

    Update env.py

--- env.py
+++ env.py
@@ -1,2 +1,2 @@
 # DATABASE_URL = 'postgresql+psycopg2://brilworks-36:brilworks@localhost:5432'
-DATABASE_URL = 'postgres://fzrposlshcrpfy:ecdebb7f0cae5ea5337aeeeb34a607ef157c35799ac625212e2f49352f5f067a@ec2-34-194-158-176.compute-1.amazonaws.com:5432/d7rstjqak7itcn'
+DATABASE_URL = 'postgresql+psycopg2:://fzrposlshcrpfy:ecdebb7f0cae5ea5337aeeeb34a607ef157c35799ac625212e2f49352f5f067a@ec2-34-194-158-176.compute-1.amazonaws.com:5432/d7rstjqak7itcn'
